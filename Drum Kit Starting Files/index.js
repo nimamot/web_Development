991cd81625f8388a6faf1cbc988cd8cb1b587e5a
nimamot <64922998+nimamot@users.noreply.github.com>
2021-08-25 16:17:43 -0700

    added the sound feature for w

--- Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/index.js
@@ -9,9 +9,12 @@
 
 for(var i=0; i< document.querySelectorAll(".drum").length; i++){
   document.querySelectorAll(".drum")[i].addEventListener("click", function (){
-    switch (expression) {
-      case expression:
 
+    var buttoninnerHTML = this.innerHTML;
+    switch (buttoninnerHTML) {
+      case "w":
+          var audio = new Audio("sounds/tom-1.mp3");
+          audio.play();
         break;
       default:
 
