5b5d567c122b9c897324d1e2d5e77de53c92678f
nimamot <64922998+nimamot@users.noreply.github.com>
2021-08-24 17:31:03 -0700

    minor changes

--- DOM/Dicee Challenge - Starting Files/index.js
+++ DOM/Dicee Challenge - Starting Files/index.js
@@ -1,2 +1,9 @@
 var randomNumber1 = Math.floor(Math.random() * 6) + 1;
 console.log(randomNumber1);
+var randomImage1 = "images/dice" + randomNumber1 + ".png";
+
+document.querySelector(".img1").setAttribute("src", randomImage1);
+document.querySelector(".img1").setAttribute("src", randomImage1);
+
+// console logs
+console.log(document.querySelector(".img1").getAttribute("src"));
