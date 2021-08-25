6829c3031d76bed2aa89d00ab277f535ddb0bd91
nimamot <64922998+nimamot@users.noreply.github.com>
2021-08-25 16:13:16 -0700

    added the switch statement in index.js

--- Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/index.js
@@ -1,5 +1,20 @@
-document.querySelector("button")
+// document.querySelector(".set").addEventListener("click", function (){
+//   alert("I got clicked!");
+// });
 
-function handleClick(){
-  alert("I got clicked!")
+// document.querySelector("button").addEventListener("click", function (){
+//   alert("I got clicked!");
+// });
+
+
+for(var i=0; i< document.querySelectorAll(".drum").length; i++){
+  document.querySelectorAll(".drum")[i].addEventListener("click", function (){
+    switch (expression) {
+      case expression:
+
+        break;
+      default:
+
+    }
+  });
 }
