97af6bc238b7dc202e037a89d7cf115a79492142
nimamot <64922998+nimamot@users.noreply.github.com>
2021-08-30 18:44:47 -0700

    minor changes on Spanish

--- spanish/index.js
+++ spanish/index.js
@@ -1,2 +1,8 @@
 var answer = document.querySelector(".cuidar");
 var button = document.querySelector(".button");
+var box = document.querySelector(".box");
+button.onclick = function(){
+  answer.innerHTML = "/ cuidar";
+  box.style.color = "green";
+
+}
