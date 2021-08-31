596040c91de651770bf40afce794838df7b14b9e
nimamot <64922998+nimamot@users.noreply.github.com>
2021-08-30 20:43:50 -0700

    box seven done (Webdevelopment/Spanish/Angelito)

--- spanish/angelito/index.js
+++ spanish/angelito/index.js
@@ -5,11 +5,19 @@ var answer1 = document.querySelector(".cuidar");
 var answer2 = document.querySelector(".guarda");
 var answer3 = document.querySelector(".llorar");
 var answer4 = document.querySelector(".marchó");
+var answer5 = document.querySelector(".hondo");
+var answer6 = document.querySelector(".avisó");
+var answer7 = document.querySelector(".roto");
+
+
 
 var box1 = document.querySelector(".box1");
 var box2 = document.querySelector(".box2");
 var box3 = document.querySelector(".box3");
 var box4 = document.querySelector(".box4");
+var box5 = document.querySelector(".box5");
+var box6 = document.querySelector(".box6");
+var box7 = document.querySelector(".box7");
 
 
 button.onclick = function(){
@@ -17,6 +25,9 @@ button.onclick = function(){
   answer2.innerHTML = "(guarda)";
   answer3.innerHTML = "(llorar)";
   answer4.innerHTML = "(marchó)";
+  answer5.innerHTML = "(hondo)";
+  answer6.innerHTML = "(avisó)";
+  answer7.innerHTML = "(roto)";
   // if Statement1
   if (box1.value == "cuidar"){
     box1.style.color = "green";
@@ -47,6 +58,30 @@ button.onclick = function(){
   }
   else{
     box4.style.color = "red";
+  }
+
+  // if Statement5
+  if (box5.value == "hondo"){
+    box5.style.color = "green";
+  }
+  else{
+    box5.style.color = "red";
+  }
+
+  // if Statement6
+  if (box6.value == "avisó"){
+    box6.style.color = "green";
+  }
+  else{
+    box6.style.color = "red";
+  }
+
+  // if Statement6
+  if (box7.value == "roto"){
+    box7.style.color = "green";
+  }
+  else{
+    box7.style.color = "red";
   }
 
 
