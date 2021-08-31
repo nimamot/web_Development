46b79ce8c84810f042a6589fc23c364f63691c92
nimamot <64922998+nimamot@users.noreply.github.com>
2021-08-30 19:21:23 -0700

    minor changes on spanish/Angelito

--- spanish/angelito/index.js
+++ spanish/angelito/index.js
@@ -1,15 +1,31 @@
-var answer = document.querySelector(".cuidar");
-var button = document.querySelector(".button");
-var box = document.querySelector(".box");
-button.onclick = function(){
-  answer.innerHTML = "/ cuidar";
 
-  if (box.value == "cuidar"){
-  box.style.color = "green";
+var button = document.querySelector(".button");
+
+var answer1 = document.querySelector(".cuidar");
+var answer2 = document.querySelector(".guarda");
+var box1 = document.querySelector(".box1");
+var box2 = document.querySelector(".box2");
+
+
+button.onclick = function(){
+  answer1.innerHTML = "(cuidar)";
+  answer2.innerHTML = "(guarda)";
+
+
+  if (box1.value == "cuidar"){
+  box1.style.color = "green";
   }
   else{
-    box.style.color = "red";
+    box1.style.color = "red";
   }
 
+  if (box2.value == "guarda"){
+  box2.style.color = "green";
+  }
+  else{
+    box2.style.color = "red";
+  }
 
 }
+
+guarda
